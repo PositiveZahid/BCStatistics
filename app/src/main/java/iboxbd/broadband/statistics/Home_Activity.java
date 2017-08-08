aff78ce2edbc80c5e115473b092fd21147f79dce
Md. Zahidul Islam <zahidulislam2008@gmail.com>
2017-08-08 18:35:56 +0600

    # Unique Device ID # Top Right Menu Item

--- app/src/main/java/iboxbd/broadband/statistics/Home_Activity.java
+++ app/src/main/java/iboxbd/broadband/statistics/Home_Activity.java
@@ -5,9 +5,12 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -34,7 +37,7 @@ public class Home_Activity extends AppCompatActivity {
         GetDB();
 
         //Connection d = new Connection();
-        try{
+        try {
 
             Connection c = new Connection();
             c.setDateTime(DateUtils.getDateTime());
@@ -55,45 +58,47 @@ public class Home_Activity extends AppCompatActivity {
 
             //for (Connection temp : _dbHelper.getAllConnections()) {
             //    Toast.makeText(this,temp.getID().toString(),Toast.LENGTH_LONG).show();
-                //temp.ID
+            //temp.ID
             //}
             //dd = _dbhandler.GetValue()
 
 
-            _dbHelper.close();
-        }catch (Exception e){
-            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+            //_dbHelper.close();
+            Toast.makeText(this, Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID), Toast.LENGTH_LONG).show();
+            //Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
         }
 
-        Button databaseButton   = (Button) findViewById(R.id.database);
-        Button startAlarm       = (Button) findViewById(R.id.alarmStart);
-        Button stopAlarm        = (Button) findViewById(R.id.alarmStop);
+        //Button databaseButton = (Button) findViewById(R.id.database);
+        //Button startAlarm       = (Button) findViewById(R.id.alarmStart);
+        //Button stopAlarm        = (Button) findViewById(R.id.alarmStop);
 
-        databaseButton.setOnClickListener(new View.OnClickListener() {
+        /*databaseButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Intent dbmanager = new Intent(Home_Activity.this,SqliteManager.class);
+                Intent dbmanager = new Intent(Home_Activity.this, SqliteManager.class);
                 startActivity(dbmanager);
             }
-        });
+        });*/
 
-        startAlarm.setOnClickListener(new View.OnClickListener() {
+        /*startAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //StartAlarm(getApplicationContext());
                 startAlert();
             }
-        });
+        });*/
 
-        stopAlarm.setOnClickListener(new View.OnClickListener() {
+        /*stopAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 StopAlarm(getApplicationContext());
             }
-        });
+        });*/
 
-        if(ServiceCheck.isMyServiceRunning(Home_Activity.this,ConnectionService.class)){
-            Toast.makeText(this,"Connection Service Running ........!!! ",Toast.LENGTH_LONG).show();
+        if (ServiceCheck.isMyServiceRunning(Home_Activity.this, ConnectionService.class)) {
+            Toast.makeText(this, "Connection Service Running ........!!! ", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -111,7 +116,7 @@ public class Home_Activity extends AppCompatActivity {
         //_dbHelper.CreateTable(new Connection());
     }
 
-    public void StartAlarm(Context context)    {
+    public void StartAlarm(Context context) {
         /*AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, RepeatReceiver.class);
         //intent.putExtra(ONE_TIME, Boolean.FALSE);
@@ -120,21 +125,21 @@ public class Home_Activity extends AppCompatActivity {
         am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60  , pi);*/
         _dbHelper = new DatabaseHelper(context);
         System.out.print("Feedback : #004 Repeat Receiver Started");
-        Log.i("#004","Repeat Receiver Started");
-        Toast.makeText(context,"Feedback : #004 Repeat Receiver Started",Toast.LENGTH_LONG).show();
-        _dbHelper.createLOG(new LogData("#004","false", DateUtils.getDateTime()));
+        Log.i("#004", "Repeat Receiver Started");
+        Toast.makeText(context, "Feedback : #004 Repeat Receiver Started", Toast.LENGTH_LONG).show();
+        _dbHelper.createLOG(new LogData("#004", "false", DateUtils.getDateTime()));
 
-        AlarmManager alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, RepeatReceiver.class);
         PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
         //alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() +                        60 * 1000, alarmIntent);
-        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,System.currentTimeMillis() , 60 * 1000 , alarmIntent);
+        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, System.currentTimeMillis(), 60 * 1000, alarmIntent);
         //alarmMgr.setInexactRepeating();
 
     }
 
-    public void StopAlarm(Context context)    {
+    public void StopAlarm(Context context) {
         Intent intent = new Intent(context, RepeatReceiver.class);
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
@@ -157,18 +162,36 @@ public class Home_Activity extends AppCompatActivity {
 
         AlarmManager am = (AlarmManager) getSystemService(ALARM_SERVICE);
 
-        long timeInterval =2 * 60 * 1000;
+        long timeInterval = 2 * 60 * 1000;
 
         am.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), timeInterval, pi);
         //am.
 
-        Toast.makeText(this, "Alarm set in " + timeInterval + " seconds",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Alarm set in " + timeInterval + " seconds", Toast.LENGTH_LONG).show();
         _dbHelper = new DatabaseHelper(this.getApplicationContext());
         System.out.println("Feedback : #004 Home Button Clicked");
-        Log.i("#004","Home Button Clicked");
-        Toast.makeText(this.getApplicationContext(),"Feedback : #004 Home Button Clicked",Toast.LENGTH_LONG).show();
-        _dbHelper.createLOG(new LogData("#004","false", DateUtils.getDateTime()));
+        Log.i("#004", "Home Button Clicked");
+        Toast.makeText(this.getApplicationContext(), "Feedback : #004 Home Button Clicked", Toast.LENGTH_LONG).show();
+        _dbHelper.createLOG(new LogData("#004", "false", DateUtils.getDateTime()));
         _dbHelper.close();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Menu item cliecked", Toast.LENGTH_LONG).show();
+                Intent dbmanager = new Intent(Home_Activity.this, SqliteManager.class);
+                startActivity(dbmanager);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
 }
